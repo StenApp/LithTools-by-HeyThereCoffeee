1a74ba54702c5b1721b84513474678ed0ef5cc40
Sten <32524257+StenApp@users.noreply.github.com>
2025-07-18 14:40:15 +0200

    Several fixes of LTA format and floatfix

--- LTDatReader/Addons/LTDatReader/Models/LTB_PS2.gd
+++ LTDatReader/Addons/LTDatReader/Models/LTB_PS2.gd
@@ -214,7 +214,18 @@ class LTB_PS2:
 			(val >> 16) & 0xFF,
 			(val >> 24) & 0xFF
 		])
-		return bytes.get_float32(0)
+		var stream = StreamPeerBuffer.new()
+		stream.data_array = bytes
+		return stream.get_float()
+		
+	# func int_to_float(val: int) -> float:
+		# var bytes = PoolByteArray([
+			# val & 0xFF,
+			# (val >> 8) & 0xFF,
+			# (val >> 16) & 0xFF,
+			# (val >> 24) & 0xFF
+		# ])
+		# return bytes.get_float32(0)
 	
 	##################
 	# Internal Classes
@@ -943,7 +954,7 @@ class LTB_PS2:
 				if fval >= 0.0 and fval <= 1.0:
 					self.value = fval
 				else:
-					self.value = rawself.value = f.get_32()
+					self.value = raw
 				
 				## For some reason they have floating point data in longint?
 				## Fix it manually...
